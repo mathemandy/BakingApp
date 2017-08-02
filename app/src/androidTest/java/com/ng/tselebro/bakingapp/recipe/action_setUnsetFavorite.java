6944ca188d59d5ad123baa3f0b6a845d53ee8c5f
mathemandy <!5ive0ero?>
2017-08-02 09:28:55 +0100

    update

--- app/src/androidTest/java/com/ng/tselebro/bakingapp/recipe/action_setUnsetFavorite.java
+++ app/src/androidTest/java/com/ng/tselebro/bakingapp/recipe/action_setUnsetFavorite.java
@@ -1,11 +1,18 @@
 package com.ng.tselebro.bakingapp.recipe;
 
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.StateListDrawable;
 import android.support.annotation.DrawableRes;
 import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.LargeTest;
+import android.support.v7.view.menu.ActionMenuItemView;
+
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
@@ -28,15 +35,17 @@ import static android.support.test.espresso.matcher.ViewMatchers.withContentDesc
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.Matchers.allOf;
 
-@LargeTest
+
 @RunWith(AndroidJUnit4.class)
-public class RecipActivity {
+public class action_setUnsetFavorite {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
     public void recipActivity_fav_icon_change() {
+
+
         ViewInteraction recyclerView = onView(
                 allOf(withId(R.id.recipeList), isDisplayed()));
         recyclerView.perform(actionOnItemAtPosition(0, click()));
@@ -53,11 +62,10 @@ public class RecipActivity {
                                         2),
                                 0),
                         isDisplayed()));
-        textView.check(matches(imageWithDrawableId(R.drawable.favorite)));
+        textView.check(matches(withActionIconDrawable(R.drawable.ic_favorite_black_24dp)));
 
-    }
-    public static DrawableMatcher imageWithDrawableId(@DrawableRes int expectedId) {
-        return new DrawableMatcher(expectedId);
+
+
     }
 
     private static Matcher<View> childAtPosition(
@@ -78,4 +86,37 @@ public class RecipActivity {
             }
         };
     }
+
+    public static Matcher<View> withActionIconDrawable(@DrawableRes final int resourceId) {
+        return new BoundedMatcher<View, ActionMenuItemView>(ActionMenuItemView.class) {
+            @Override
+            public void describeTo(final Description description) {
+                description.appendText("has image drawable resource " + resourceId);
+            }
+
+            @Override
+            public boolean matchesSafely(final ActionMenuItemView actionMenuItemView) {
+                //noinspection RestrictedApi
+                return sameBitmap(actionMenuItemView.getContext(), actionMenuItemView.getItemData().getIcon(), resourceId);
+            }
+        };
+    }
+
+    private static boolean sameBitmap(Context context, Drawable drawable, int resourceId) {
+        Drawable otherDrawable = context.getResources().getDrawable(resourceId);
+        if (drawable == null || otherDrawable == null) {
+            return false;
+        }
+        if (drawable instanceof StateListDrawable && otherDrawable instanceof StateListDrawable) {
+            drawable = drawable.getCurrent();
+            otherDrawable = otherDrawable.getCurrent();
+        }
+        if (drawable instanceof BitmapDrawable) {
+            Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+            Bitmap otherBitmap = ((BitmapDrawable) otherDrawable).getBitmap();
+            return bitmap.sameAs(otherBitmap);
+        }
+        return false;
+    }
 }
+
